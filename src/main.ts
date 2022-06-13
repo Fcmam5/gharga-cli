af30c911b228fa5a550709f605c67c92320ebb44
Abdeldjalil Fortas <dr.fcmam5@gmail.com>
2022-06-14 00:08:35 +0200

    Attempt to move files; please work

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { renderTableString } from "./lib/write-doc";
 import { loadActionAsJSON } from "./lib/read-yaml";
 
-const fileContent = loadActionAsJSON("./.github/workflows/example.yaml");
+const fileContent = loadActionAsJSON("./.github/workflows/00-example.yaml");
 
 console.log(renderTableString(fileContent));
